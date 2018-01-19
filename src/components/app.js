0c7a36bbb4296f827216725ab189d8ed00ff7d69
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-19 14:55:26 -0800

    finished navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import logo from '../assets/images/logo.svg';
+import Nav from './nav';
 
 const App = () => (
     <div>
+        <Nav/>
         <h1 className='center-align'>User Auth Demo</h1>
     </div>
 );
