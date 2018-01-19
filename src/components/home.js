85b506ffe8faf35c8730600fdbd79f1a3fc74482
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-19 15:26:45 -0800

    routing complete

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props =>{
+    return(
+        <div>
+            <h1 className="center-align">Welcome to Movie Qutoes</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse eveniet eaque deserunt blanditiis at.</p>
+        </div>
+    )
+}
